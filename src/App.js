2a17b6d3bb57adb2102d41b11e4c38712379f2f6
João Antunes <joao.antunes@efabrika.com.br>
2021-08-14 14:43:21 -0300

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import _ from 'lodash';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import SendIcon from '@material-ui/icons/Send';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [tarefas, setTarefas] = useState([]);
+  const [novaTarefa, setNovaTarefa] = useState(''); 
+  
+  const handleSetJobs = e => {
+    e.preventDefault();
+    if (novaTarefa !== ''){
+      setTarefas([...tarefas, novaTarefa]);
+      setNovaTarefa('');
+    }
+  }
+
+  const handleChange = e => setNovaTarefa(e.target.value);
+
+  const handleDelete = index => {
+    const novasTarefas = _.remove(tarefas, (t, i) => {
+      return i !== index;
+    } )
+    setTarefas(novasTarefas);
+  } 
+
+  return (  
+     <div className="box">
+       
+       <h1>Lista de Tarefas</h1>
+      
+        <Paper elevation={3} className="paper">
+
+          <form className="form" onSubmit={handleSetJobs}>
+            
+            <div className="input">
+            <TextField               
+              label="Tarefa" 
+              variant="outlined"
+              value={novaTarefa}
+              onChange={handleChange}
+              fullWidth="true"
+            />          
+
+            <Button 
+              className="button"
+              color="primary"
+              type="submit"
+              fontSize="large"
+            >
+              <SendIcon color="primary" fontSize="large" />
+            </Button>
+            </div>
+
+          </form>
+
+            <Table aria-label="customized table">
+              <TableHead className='head'>
+                <TableRow>
+                  <TableCell><strong>TAREFAS</strong></TableCell>
+                  <TableCell align="right"><strong>DELETAR</strong></TableCell>
+                </TableRow>
+              </TableHead>
+
+              <TableBody>
+                {tarefas.map((tarefa, index) => (
+                  <TableRow key={index}>
+                    <TableCell>{tarefa}</TableCell>
+                    <TableCell align="right">
+                    <IconButton  color="primary" aria-label="upload picture" component="span">
+                    <DeleteIcon
+                        onClick={() => handleDelete(index)}                        
+                        color="secondary"                       
+                    />
+                    </IconButton>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+
+        </Paper>
     </div>
   );
 }
